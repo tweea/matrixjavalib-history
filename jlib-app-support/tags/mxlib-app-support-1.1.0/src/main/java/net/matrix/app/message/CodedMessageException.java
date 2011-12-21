5e66b0f9ce1f4a62fa6c9cd7273dadb2f0f29d33
tweea <tweea@8c84e9a4-e2ed-cfba-bff3-1f46ce8ca838>
2011-12-21 05:59:18 +0000

    [maven-release-plugin]  copy for tag mxlib-app-support-1.1.0

---
+++ jlib-app-support/tags/mxlib-app-support-1.1.0/src/main/java/net/matrix/app/message/CodedMessageException.java
@@ -0,0 +1,56 @@
+/*
+ * $Id$
+ * Copyright(C) 2009 北航冠新
+ * All right reserved.
+ */
+package net.matrix.app.message;
+
+import net.matrix.app.SystemException;
+
+/**
+ * 配置错误
+ */
+public class CodedMessageException
+	extends SystemException
+{
+	private static final long serialVersionUID = 8050980676195083467L;
+
+	public CodedMessageException()
+	{
+		super();
+	}
+
+	public CodedMessageException(Throwable cause)
+	{
+		super(cause);
+	}
+
+	public CodedMessageException(CodedMessage rootMessage)
+	{
+		super(rootMessage);
+	}
+
+	public CodedMessageException(String rootMessageCode)
+	{
+		super(rootMessageCode);
+	}
+
+	public CodedMessageException(Throwable cause, CodedMessage rootMessage)
+	{
+		super(cause, rootMessage);
+	}
+
+	public CodedMessageException(Throwable cause, String rootMessageCode)
+	{
+		super(cause, rootMessageCode);
+	}
+
+	/**
+	 * @return 根消息
+	 */
+	@Override
+	public String getDefaultMessageCode()
+	{
+		return "CodedMessage.Error";
+	}
+}
