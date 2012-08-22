4ec5a4ded69aa05b912625dff872e45044dcbab0
tweea <tweea@8c84e9a4-e2ed-cfba-bff3-1f46ce8ca838>
2012-08-22 02:03:03 +0000

    [maven-release-plugin]  copy for tag mxlib-app-support-1.1.3

---
+++ main/tags/mxlib-app-support-1.1.3/src/test/java/net/matrix/app/message/CodedMessageListTest.java
@@ -0,0 +1,75 @@
+/*
+ * $Id$
+ * Copyright(C) 2009 matrix
+ * All right reserved.
+ */
+package net.matrix.app.message;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class CodedMessageListTest {
+	@Test
+	public void logMessageList() {
+		CodedMessageList messageList = new CodedMessageList();
+		Assert.assertEquals(0, messageList.size());
+	}
+
+	@Test
+	public void addLogMessage() {
+		CodedMessageList messageList = new CodedMessageList();
+		Assert.assertEquals(0, messageList.size());
+		CodedMessage message = new CodedMessage("System.Error", CodedMessageLevels.INFORMATION);
+		messageList.add(message);
+		Assert.assertEquals(1, messageList.size());
+		Assert.assertEquals(message, messageList.get(0));
+	}
+
+	@Test
+	public void addLogMessageList() {
+		CodedMessageList messageList = new CodedMessageList();
+		CodedMessageList messageList2 = new CodedMessageList();
+		CodedMessage message = new CodedMessage("System.Error", CodedMessageLevels.INFORMATION);
+		messageList.add(message);
+		messageList2.add(message);
+		messageList2.addAll(messageList);
+		Assert.assertEquals(2, messageList2.size());
+		Assert.assertEquals(message, messageList2.get(1));
+	}
+
+	@Test
+	public void save()
+		throws IOException, CodedMessageException {
+		CodedMessageList messageList = new CodedMessageList();
+		CodedMessage message = new CodedMessage("System.Error", CodedMessageLevels.INFORMATION);
+		message.addArgument("test1");
+		message.addArgument("test2");
+		messageList.add(message);
+		message = new CodedMessage("100000000", CodedMessageLevels.INFORMATION);
+		message.addArgument("test3");
+		messageList.add(message);
+		StringWriter os = new StringWriter();
+		messageList.save(os);
+		os.close();
+		// 读取
+		Reader is = new StringReader(os.toString());
+		CodedMessageList messageList2 = CodedMessageList.load(is);
+		is.close();
+		Assert.assertEquals(messageList.size(), messageList2.size());
+		for (int index = 0; index < messageList.size(); index++) {
+			CodedMessage message1 = messageList.get(index);
+			CodedMessage message2 = messageList2.get(index);
+			Assert.assertEquals(message1.getCode(), message2.getCode());
+			Assert.assertEquals(message1.getTime(), message2.getTime());
+			Assert.assertEquals(message1.getArguments().size(), message2.getArguments().size());
+		}
+	}
+}
