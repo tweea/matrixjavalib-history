5530e1b02f1ecc3b050c1870a49f20de13318163
tweea <tweea@8c84e9a4-e2ed-cfba-bff3-1f46ce8ca838>
2013-03-05 16:36:47 +0000

    补充测试

--- main/trunk/lib-common/src/test/java/net/matrix/text/ResourcesTest.java
+++ main/trunk/lib-common/src/test/java/net/matrix/text/ResourcesTest.java
@@ -1,5 +1,11 @@
+/*
+ * $Id$
+ * Copyright(C) 2008 Matrix
+ * All right reserved.
+ */
 package net.matrix.text;
 
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import org.junit.Assert;
@@ -10,5 +16,19 @@ public class ResourcesTest {
 	public void testGetBundle() {
 		ResourceBundle bundle = Resources.getBundle("global");
 		Assert.assertEquals("男性", bundle.getString("male"));
+	}
+
+	@Test
+	public void testGetBundle_locale() {
+		ResourceBundle bundle = Resources.getBundle("global", Locale.US);
+		Assert.assertEquals("爷们", bundle.getString("male"));
+	}
+
+	@Test
+	public void testGetProperty() {
+		ResourceBundle bundle = Resources.getBundle("global", Locale.US);
+		Assert.assertEquals("爷们", Resources.getProperty(bundle, "male"));
+		Assert.assertEquals("yes", Resources.getProperty(bundle, "yes"));
+		Assert.assertEquals("OK", Resources.getProperty(bundle, "OK"));
 	}
 }
