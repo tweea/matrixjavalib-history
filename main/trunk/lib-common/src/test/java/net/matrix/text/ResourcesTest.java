5c59db11c8fca0d0148dda74a1227fe826c22c56
tweea <tweea@8c84e9a4-e2ed-cfba-bff3-1f46ce8ca838>
2013-03-06 03:18:38 +0000

    增加当前区域设置

--- main/trunk/lib-common/src/test/java/net/matrix/text/ResourcesTest.java
+++ main/trunk/lib-common/src/test/java/net/matrix/text/ResourcesTest.java
@@ -14,6 +14,7 @@ import org.junit.Test;
 public class ResourcesTest {
 	@Test
 	public void testGetBundle() {
+		Locales.current(Locale.CHINA);
 		ResourceBundle bundle = Resources.getBundle("global");
 		Assert.assertEquals("男性", bundle.getString("male"));
 	}
@@ -36,5 +37,22 @@ public class ResourcesTest {
 		Assert.assertEquals("爷们", Resources.getProperty(bundle, "male"));
 		Assert.assertEquals("yes", Resources.getProperty(bundle, "yes"));
 		Assert.assertEquals("OK", Resources.getProperty(bundle, "OK"));
+	}
+
+	@Test
+	public void testFormatMessage() {
+		ResourceBundle bundle = Resources.getBundle("global", Locale.CHINA);
+		Assert.assertEquals("一双绣花鞋", Resources.formatMessage(bundle, "message", "绣花鞋"));
+	}
+
+	@Test
+	public void testFormatMessage_fallback() {
+		ResourceBundle bundle = Resources.getBundle("global", Locale.CHINA);
+		Assert.assertEquals("1, 2, 3", Resources.formatMessage(bundle, "1", "2", 3));
+	}
+
+	@Test
+	public void testFallbackFormatMessage() {
+		Assert.assertEquals("1, 2, 3", Resources.formatFallbackMessage("1", "2", 3));
 	}
 }
