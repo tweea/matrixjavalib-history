c2882374c91c5bfa92a9c25a88067aa76a7e799c
tweea <tweea@263.net>
2013-03-06 08:37:30 +0000

    [maven-release-plugin]  copy for tag mxlib-common-1.5.0

---
+++ main/tags/mxlib-common-1.5.0/src/main/java/net/matrix/util/Threads.java
@@ -0,0 +1,116 @@
+/*
+ * $Id$
+ * Copyright(C) 2011 Matrix
+ * All right reserved.
+ */
+package net.matrix.util;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 线程相关工具。
+ */
+public final class Threads {
+	/**
+	 * 日志记录器。
+	 */
+	private static final Logger LOG = LoggerFactory.getLogger(Threads.class);
+
+	/**
+	 * 阻止实例化。
+	 */
+	private Threads() {
+	}
+
+	/**
+	 * sleep 等待，忽略 InterruptedException。
+	 * 
+	 * @param millis
+	 *            等待毫秒数
+	 */
+	public static void sleep(final long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			LOG.warn("thread interrupted.");
+		}
+	}
+
+	/**
+	 * sleep 等待，忽略 InterruptedException。
+	 * 
+	 * @param duration
+	 *            等待时间
+	 * @param unit
+	 *            时间单位
+	 */
+	public static void sleep(final long duration, final TimeUnit unit) {
+		try {
+			Thread.sleep(unit.toMillis(duration));
+		} catch (InterruptedException e) {
+			LOG.warn("thread interrupted.");
+		}
+	}
+
+	/**
+	 * 按照 ExecutorService JavaDoc 示例代码编写的 Graceful Shutdown 方法。
+	 * 先使用 shutdown，停止接收新任务并尝试完成所有已存在任务。
+	 * 如果超时，则调用 shutdownNow，取消在 workQueue 中 Pending 的任务，并中断所有阻塞函数。
+	 * 如果仍然超時，則強制退出。
+	 * 另对在 shutdown 时线程本身被调用中断做了处理。
+	 * 
+	 * @param pool
+	 *            ExecutorService
+	 * @param shutdownTimeout
+	 *            等待关闭超时时间
+	 * @param shutdownNowTimeout
+	 *            立即关闭超时时间
+	 * @param timeUnit
+	 *            时间单位
+	 */
+	public static void gracefulShutdown(final ExecutorService pool, final int shutdownTimeout, final int shutdownNowTimeout, final TimeUnit timeUnit) {
+		// Disable new tasks from being submitted
+		pool.shutdown();
+		try {
+			// Wait a while for existing tasks to terminate
+			if (!pool.awaitTermination(shutdownTimeout, timeUnit)) {
+				// Cancel currently executing tasks
+				pool.shutdownNow();
+				// Wait a while for tasks to respond to being cancelled
+				if (!pool.awaitTermination(shutdownNowTimeout, timeUnit)) {
+					LOG.error("Pool did not terminated");
+				}
+			}
+		} catch (InterruptedException ie) {
+			// (Re-)Cancel if current thread also interrupted
+			pool.shutdownNow();
+			// Preserve interrupt status
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	/**
+	 * 直接调用 shutdownNow 的方法，有 timeout 控制。取消在 workQueue 中 Pending 的任务，并中断所有阻塞函数。
+	 * 
+	 * @param pool
+	 *            ExecutorService
+	 * @param timeout
+	 *            立即关闭超时时间
+	 * @param timeUnit
+	 *            时间单位
+	 */
+	public static void normalShutdown(final ExecutorService pool, final int timeout, final TimeUnit timeUnit) {
+		try {
+			pool.shutdownNow();
+			if (!pool.awaitTermination(timeout, timeUnit)) {
+				LOG.error("Pool did not terminated");
+			}
+		} catch (InterruptedException ie) {
+			Thread.currentThread().interrupt();
+		}
+	}
+}
---
+++ main/tags/mxlib-common-1.5.0/src/test/java/net/matrix/text/ResourcesTest.java
@@ -0,0 +1,41 @@
+/*
+ * $Id$
+ * Copyright(C) 2008 Matrix
+ * All right reserved.
+ */
+package net.matrix.text;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ResourcesTest {
+	@Test
+	public void testGetBundle() {
+		Locales.current(Locale.CHINA);
+		ResourceBundle bundle = Resources.getBundle("global");
+		Assert.assertEquals("男性", bundle.getString("male"));
+	}
+
+	@Test
+	public void testGetBundle_locale() {
+		ResourceBundle bundle = Resources.getBundle("global", Locale.US);
+		Assert.assertEquals("爷们", bundle.getString("male"));
+	}
+
+	@Test
+	public void testGetBundle_fallback() {
+		ResourceBundle bundle = Resources.getBundle("fallback");
+		Assert.assertEquals("male", bundle.getString("male"));
+	}
+
+	@Test
+	public void testGetProperty() {
+		ResourceBundle bundle = Resources.getBundle("global", Locale.US);
+		Assert.assertEquals("爷们", Resources.getProperty(bundle, "male"));
+		Assert.assertEquals("yes", Resources.getProperty(bundle, "yes"));
+		Assert.assertEquals("OK", Resources.getProperty(bundle, "OK"));
+	}
+}
