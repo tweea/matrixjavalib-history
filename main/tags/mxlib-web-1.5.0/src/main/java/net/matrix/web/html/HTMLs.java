1b0ec287f3b0c103f79b514d7b8f25ebd95d8943
tweea <tweea@8c84e9a4-e2ed-cfba-bff3-1f46ce8ca838>
2013-03-07 06:47:16 +0000

    [maven-release-plugin]  copy for tag mxlib-web-1.5.0

---
+++ main/tags/mxlib-web-1.5.0/src/main/java/net/matrix/web/html/HTMLs.java
@@ -0,0 +1,42 @@
+/*
+ * $Id$
+ * Copyright(C) 2008 Matrix
+ * All right reserved.
+ */
+package net.matrix.web.html;
+
+/**
+ * HTML 相关工具。
+ */
+public final class HTMLs {
+	/**
+	 * 空格。
+	 */
+	public static final String SPACE = "&nbsp;";
+
+	/**
+	 * 阻止实例化。
+	 */
+	private HTMLs() {
+	}
+
+	public static String fitToLength(final String str, final int length) {
+		StringBuilder sb = new StringBuilder();
+		if (str == null) {
+			for (int i = 0; i < length; i++) {
+				sb.append(SPACE);
+			}
+			return sb.toString();
+		}
+		int len = str.length();
+		if (len >= length) {
+			return str;
+		} else {
+			sb.append(str);
+			for (int i = 0; i < length - len; i++) {
+				sb.append(SPACE);
+			}
+			return sb.toString();
+		}
+	}
+}
---
+++ main/tags/mxlib-web-1.5.0/src/test/java/net/matrix/web/html/HTMLsTest.java
@@ -0,0 +1,18 @@
+/*
+ * $Id$
+ * Copyright(C) 2011 matrix
+ * All right reserved.
+ */
+package net.matrix.web.html;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HTMLsTest {
+	@Test
+	public void fitToLength() {
+		String xx = "abc";
+		String yy = "abc&nbsp;&nbsp;";
+		Assert.assertEquals(yy, HTMLs.fitToLength(xx, 5));
+	}
+}
