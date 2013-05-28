417e011dc50ca73575314664a3ec58faed1492c9
tweea <tweea@8c84e9a4-e2ed-cfba-bff3-1f46ce8ca838>
2013-05-28 02:25:38 +0000

    [maven-release-plugin] copy for tag mxlib-common-1.5.1

---
+++ main/tags/mxlib-common-1.5.1/src/test/java/net/matrix/util/EncodesTest.java
@@ -0,0 +1,24 @@
+package net.matrix.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EncodesTest {
+	@Test
+	public void base62Encode() {
+		long num = 63;
+
+		String result = Encodes.encodeBase62(num);
+		Assert.assertEquals("11", result);
+		Assert.assertEquals(num, Encodes.decodeBase62(result));
+	}
+
+	@Test
+	public void urlEncode() {
+		String input = "http://locahost/?q=中文&t=1";
+		String result = Encodes.urlEncode(input);
+		System.out.println(result);
+
+		Assert.assertEquals(input, Encodes.urlDecode(result));
+	}
+}
