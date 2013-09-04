505f874c1e703157e5892d523fa2f19e3b0d2ba9
tweea@263.net <tweea@263.net>
2013-09-04 09:46:39 +0000

    [maven-release-plugin]  copy for tag mxlib-common-1.6.0

---
+++ main/tags/mxlib-common-1.6.0/src/test/java/net/matrix/lang/Strings2Test.java
@@ -0,0 +1,16 @@
+/*
+ * $Id$
+ * 版权所有 2013 Matrix。
+ * 保留所有权利。
+ */
+package net.matrix.lang;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Strings2Test {
+	@Test
+	public void testReplaceAllBetweenDelimiter() {
+		Assert.assertEquals("abc<Good>xyz", Strings2.replaceAllBetweenDelimiter("abc<Bad>xyz", "<", ">", "Bad", "Good"));
+	}
+}
