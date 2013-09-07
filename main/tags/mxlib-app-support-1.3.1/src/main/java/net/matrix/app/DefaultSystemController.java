3c2fb0d0a9c27e83791cee957fd6d876d7dda508
tweea@263.net <tweea@263.net>
2013-09-07 11:29:02 +0000

    [maven-release-plugin]  copy for tag mxlib-app-support-1.3.1

---
+++ main/tags/mxlib-app-support-1.3.1/src/main/java/net/matrix/app/DefaultSystemController.java
@@ -0,0 +1,43 @@
+/*
+ * $Id$
+ * Copyright(C) 2008 Matrix
+ * All right reserved.
+ */
+package net.matrix.app;
+
+/**
+ * 默认的系统控制器，在应用中继承本类以扩展功能。
+ */
+public class DefaultSystemController
+	implements SystemController {
+	/**
+	 * 关联的系统环境。
+	 */
+	private SystemContext context;
+
+	@Override
+	public void setContext(final SystemContext context) {
+		this.context = context;
+	}
+
+	@Override
+	public SystemContext getContext() {
+		return context;
+	}
+
+	@Override
+	public void init() {
+	}
+
+	@Override
+	public void start() {
+	}
+
+	@Override
+	public void stop() {
+	}
+
+	@Override
+	public void reset() {
+	}
+}
