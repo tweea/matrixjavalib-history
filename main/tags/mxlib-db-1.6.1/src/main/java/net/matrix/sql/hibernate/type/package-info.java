ddc1187c11758a32eeefb82dff3f33c25ddfdca7
tweea@263.net <tweea@263.net>
2013-09-07 09:13:49 +0000

    [maven-release-plugin]  copy for tag mxlib-db-1.6.1

---
+++ main/tags/mxlib-db-1.6.1/src/main/java/net/matrix/sql/hibernate/type/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * $Id$
+ * Copyright(C) 2009 matrix
+ * All right reserved.
+ */
+/**
+ * Hibernate 自定义数据类型。
+ */
+// TODO use org.jadira.usertype.spi
+package net.matrix.sql.hibernate.type;
+
