070a95fb4755329c8ea2bb40bb86634133731def
tweea@263.net <tweea@263.net@8c84e9a4-e2ed-cfba-bff3-1f46ce8ca838>
2013-09-27 08:46:40 +0000

    [maven-release-plugin]  copy for tag mxlib-db-1.6.2

---
+++ main/tags/mxlib-db-1.6.2/src/test/java/net/matrix/sql/hibernate/HibernateHelperTest.java
@@ -0,0 +1,32 @@
+/*
+ * $Id$
+ * Copyright(C) 2008 Matrix
+ * All right reserved.
+ */
+package net.matrix.sql.hibernate;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @since 2007 三月 1
+ */
+public class HibernateHelperTest {
+	@BeforeClass
+	public static void setUpBeforeClass()
+		throws Exception {
+		SessionFactoryManager.getInstance();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass()
+		throws Exception {
+	}
+
+	@Test
+	public void testSet()
+		throws Exception {
+		HibernateHelper.querySQLAsMap("VALUES CURRENT_DATE");
+	}
+}
