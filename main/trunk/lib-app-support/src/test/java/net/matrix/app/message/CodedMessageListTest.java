a4d16af61dc8cebf8bd064e8fec87a15e33351e2
tweea@263.net <tweea@263.net>
2013-12-27 01:56:30 +0000

    使应用消息支持依附消息，将级联异常信息封装为单独消息

--- main/trunk/lib-app-support/src/test/java/net/matrix/app/message/CodedMessageListTest.java
+++ main/trunk/lib-app-support/src/test/java/net/matrix/app/message/CodedMessageListTest.java
@@ -5,11 +5,6 @@
  */
 package net.matrix.app.message;
 
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
-import java.io.StringWriter;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -40,33 +35,5 @@ public class CodedMessageListTest {
 		messageList2.addAll(messageList);
 		Assert.assertEquals(2, messageList2.size());
 		Assert.assertEquals(message, messageList2.get(1));
-	}
-
-	@Test
-	public void save()
-		throws IOException, CodedMessageException {
-		CodedMessageList messageList = new CodedMessageList();
-		CodedMessage message = CodedMessages.information("System.Error");
-		message.addArgument("test1");
-		message.addArgument("test2");
-		messageList.add(message);
-		message = CodedMessages.information("100000000");
-		message.addArgument("test3");
-		messageList.add(message);
-		StringWriter os = new StringWriter();
-		messageList.save(os);
-		os.close();
-		// 读取
-		Reader is = new StringReader(os.toString());
-		CodedMessageList messageList2 = CodedMessageList.load(is);
-		is.close();
-		Assert.assertEquals(messageList.size(), messageList2.size());
-		for (int index = 0; index < messageList.size(); index++) {
-			CodedMessage message1 = messageList.get(index);
-			CodedMessage message2 = messageList2.get(index);
-			Assert.assertEquals(message1.getCode(), message2.getCode());
-			Assert.assertEquals(message1.getTime(), message2.getTime());
-			Assert.assertEquals(message1.getArguments().size(), message2.getArguments().size());
-		}
 	}
 }
---
+++ main/trunk/lib-app-support/src/test/java/net/matrix/app/message/CodedMessagesTest.java
@@ -0,0 +1,44 @@
+/*
+ * $Id$
+ * Copyright(C) 2009 matrix
+ * All right reserved.
+ */
+package net.matrix.app.message;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CodedMessagesTest {
+	@Test
+	public void save()
+		throws IOException, CodedMessageException {
+		CodedMessageList messageList = new CodedMessageList();
+		CodedMessage message = CodedMessages.information("System.Error");
+		message.addArgument("test1");
+		message.addArgument("test2");
+		messageList.add(message);
+		message = CodedMessages.information("100000000");
+		message.addArgument("test3");
+		messageList.add(message);
+		StringWriter os = new StringWriter();
+		CodedMessages.save(messageList, os);
+		os.close();
+		// 读取
+		Reader is = new StringReader(os.toString());
+		CodedMessageList messageList2 = CodedMessages.load(is);
+		is.close();
+		Assert.assertEquals(messageList.size(), messageList2.size());
+		for (int index = 0; index < messageList.size(); index++) {
+			CodedMessage message1 = messageList.get(index);
+			CodedMessage message2 = messageList2.get(index);
+			Assert.assertEquals(message1.getCode(), message2.getCode());
+			Assert.assertEquals(message1.getTime(), message2.getTime());
+			Assert.assertEquals(message1.getArguments().size(), message2.getArguments().size());
+		}
+	}
+}
