41fa74ab2b5725f73af8026953a46f9e9ef8ebb8
tweea@263.net <tweea@263.net@8c84e9a4-e2ed-cfba-bff3-1f46ce8ca838>
2013-12-27 06:21:10 +0000

    使应用消息支持依附消息，将级联异常信息封装为单独消息

--- main/trunk/lib-app-support/src/test/java/net/matrix/app/SystemExceptionTest.java
+++ main/trunk/lib-app-support/src/test/java/net/matrix/app/SystemExceptionTest.java
@@ -15,40 +15,40 @@ public class SystemExceptionTest {
 	@Test
 	public void testSystemException() {
 		SystemException exception = new SystemException();
-		Assert.assertEquals("System.Error", exception.getRootMessage().getCode());
-		Assert.assertEquals(CodedMessageLevel.ERROR, exception.getRootMessage().getLevel());
-		Assert.assertEquals(1, exception.getMessageList().size());
+		Assert.assertEquals("System.Error", exception.getCodedMessage().getCode());
+		Assert.assertEquals(CodedMessageLevel.ERROR, exception.getCodedMessage().getLevel());
+		Assert.assertEquals(0, exception.getCodedMessage().getMessages().size());
 	}
 
 	@Test
 	public void testSystemException2() {
 		SystemException exception = new SystemException(new Exception());
-		Assert.assertEquals("System.Error", exception.getRootMessage().getCode());
-		Assert.assertEquals(CodedMessageLevel.ERROR, exception.getRootMessage().getLevel());
-		Assert.assertEquals(1, exception.getMessageList().size());
+		Assert.assertEquals("System.Error", exception.getCodedMessage().getCode());
+		Assert.assertEquals(CodedMessageLevel.ERROR, exception.getCodedMessage().getLevel());
+		Assert.assertEquals(0, exception.getCodedMessage().getMessages().size());
 		exception = new SystemException(exception);
-		Assert.assertEquals("System.Error", exception.getRootMessage().getCode());
-		Assert.assertEquals(CodedMessageLevel.ERROR, exception.getRootMessage().getLevel());
-		Assert.assertEquals(2, exception.getMessageList().size());
+		Assert.assertEquals("System.Error", exception.getCodedMessage().getCode());
+		Assert.assertEquals(CodedMessageLevel.ERROR, exception.getCodedMessage().getLevel());
+		Assert.assertEquals(1, exception.getCodedMessage().getMessages().size());
 	}
 
 	@Test
 	public void testSystemException3() {
 		SystemException exception = new SystemException(CodedMessages.information("System.Error"));
-		Assert.assertEquals("System.Error", exception.getRootMessage().getCode());
-		Assert.assertEquals(CodedMessageLevel.INFORMATION, exception.getRootMessage().getLevel());
-		Assert.assertEquals(1, exception.getMessageList().size());
+		Assert.assertEquals("System.Error", exception.getCodedMessage().getCode());
+		Assert.assertEquals(CodedMessageLevel.INFORMATION, exception.getCodedMessage().getLevel());
+		Assert.assertEquals(0, exception.getCodedMessage().getMessages().size());
 	}
 
 	@Test
 	public void testSystemException4() {
 		SystemException exception = new SystemException(new Exception(), CodedMessages.information("System.Error"));
-		Assert.assertEquals("System.Error", exception.getRootMessage().getCode());
-		Assert.assertEquals(CodedMessageLevel.INFORMATION, exception.getRootMessage().getLevel());
-		Assert.assertEquals(2, exception.getMessageList().size());
+		Assert.assertEquals("System.Error", exception.getCodedMessage().getCode());
+		Assert.assertEquals(CodedMessageLevel.INFORMATION, exception.getCodedMessage().getLevel());
+		Assert.assertEquals(0, exception.getCodedMessage().getMessages().size());
 		exception = new SystemException(exception, CodedMessages.information("System.Error"));
-		Assert.assertEquals("System.Error", exception.getRootMessage().getCode());
-		Assert.assertEquals(CodedMessageLevel.INFORMATION, exception.getRootMessage().getLevel());
-		Assert.assertEquals(3, exception.getMessageList().size());
+		Assert.assertEquals("System.Error", exception.getCodedMessage().getCode());
+		Assert.assertEquals(CodedMessageLevel.INFORMATION, exception.getCodedMessage().getLevel());
+		Assert.assertEquals(1, exception.getCodedMessage().getMessages().size());
 	}
 }
