38d82c50c5bc417a2e4a2a33d28ab2d5e58c75b6
tweea@263.net <tweea@263.net@8c84e9a4-e2ed-cfba-bff3-1f46ce8ca838>
2014-01-23 08:55:45 +0000

    [maven-release-plugin]  copy for tag mxlib-db-1.7.0

---
+++ main/tags/mxlib-db-1.7.0/src/main/java/net/matrix/sql/hibernate/HQLs.java
@@ -0,0 +1,29 @@
+/*
+ * $Id$
+ * Copyright(C) 2008 Matrix
+ * All right reserved.
+ */
+package net.matrix.sql.hibernate;
+
+/**
+ * HQL 工具类。
+ */
+public final class HQLs {
+	private static final char[] PARAMETER_PREFIX = {
+		':', 'p'
+	};
+
+	/**
+	 * 阻止实例化。
+	 */
+	private HQLs() {
+	}
+
+	public static void appendParameterName(StringBuilder sb, int index) {
+		sb.append(PARAMETER_PREFIX).append(index);
+	}
+
+	public static String getParameterName(int index) {
+		return PARAMETER_PREFIX[1] + Integer.toString(index);
+	}
+}
