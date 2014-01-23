e97275a8c52fe9e0770fccdf37c344bb8852a6a5
tweea@263.net <tweea@263.net@8c84e9a4-e2ed-cfba-bff3-1f46ce8ca838>
2014-01-23 06:38:47 +0000

    [maven-release-plugin]  copy for tag mxlib-app-support-1.7.0

---
+++ main/tags/mxlib-app-support-1.7.0/src/main/java/net/matrix/app/CodedException.java
@@ -0,0 +1,27 @@
+/*
+ * $Id$
+ * 版权所有 2012 Matrix。
+ * 保留所有权利。
+ */
+package net.matrix.app;
+
+import net.matrix.app.message.CodedMessage;
+
+/**
+ * 包含编码消息的异常。
+ */
+public interface CodedException {
+	/**
+	 * 获取异常的默认消息编码。
+	 * 
+	 * @return 默认消息编码
+	 */
+	String getDefaultMessageCode();
+
+	/**
+	 * 获取编码消息。
+	 * 
+	 * @return 编码消息
+	 */
+	CodedMessage getCodedMessage();
+}
